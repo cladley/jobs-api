4099f782100fb5ef832ceaae510069701878ccc6
Colin Ladley <colin.ladley@dotcraft.agency>
2024-11-18 17:54:36 +0000

    Another commit

--- app.js
+++ app.js
@@ -26,11 +26,16 @@ app.use(
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
-app.use(xss);
+app.use(xss());
 
 // routes
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUserMiddleware, jobsRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my app");
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
